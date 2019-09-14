22a3208c9065c11cc36cab8f5723228d434156cd
Matthew Wong <mwon3919@gmail.com>
2019-09-14 11:38:32 -0400

    first commit

---
+++ lesson1.py
@@ -0,0 +1,37 @@
+#computation in Python
+print(2 ** 3)
+
+
+# variables, storing values in names
+age = 23  # integer value
+print(age)
+
+name = 'Matthew'  # exact text is a string object
+print(name)
+
+# boolean variable, is either True or False
+isPythonFun = True  # boolean value
+print(isPythonFun)
+
+# naming variable
+is_python_programming_fun = True
+print(is_python_programming_fun)
+
+''' use variables we want to be descriptive and neat. keep short if you can, tells you something about the code'''
+
+
+# modulo division
+print(12 % 2)  # 2 goes into 12 perfectly, no remainder
+print(12 % 3)  # 3 goes into 12 perfectly, no remainder
+print(12 % 5)  # 5 goes into 12 2 times, 2 remainder
+
+# forward division
+print(12 // 5)  # returns the first whole number, 12/5 = 2.5 but // gives 2
+
+# library called Math. Contains useful functions that you can use on python.
+import math
+print(math.sin(12))
+
+
+mathValue = math.sin(math.pi / 2)
+print(mathValue)
